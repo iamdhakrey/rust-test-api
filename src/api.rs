26c51c7d143f179fc73903bf96ba8e0c2b0175d0
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2024-03-02 08:32:44 +0530

    add api echo and hello test case

--- src/api.rs
+++ src/api.rs
@@ -13,3 +13,36 @@ pub async fn echo(req_body: String) -> impl Responder {
 pub async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, App};
+
+    use super::*;
+    #[actix_web::test]
+    async fn test_hello() {
+        let app = test::init_service(App::new().service(hello)).await;
+
+        let req = test::TestRequest::get().uri("/").to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+        let body = test::read_body(resp).await;
+        assert_eq!(body, "Hello World!");
+    }
+
+    #[actix_web::test]
+    async fn test_echo() {
+        let app = test::init_service(App::new().service(echo)).await;
+
+        let req = test::TestRequest::post()
+            .uri("/echo")
+            .set_payload("Test")
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+
+        assert!(resp.status().is_success());
+        let body = test::read_body(resp).await;
+        assert_eq!(body, "Test");
+    }
+}
