95053899650462742b32b95746bc48a3494ebb58
Hrithik Dhakrey <dhakreyhrithik@gmail.com>
2024-03-04 18:09:12 +0530

    add counter api

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use dotenv::dotenv;
-use std::env;
+use std::{env, sync::Mutex};
 
 mod api;
+mod json;
 mod logger;
 mod routes;
-
 use routes::init_routes;
 
 use logger::logs;
@@ -25,14 +25,34 @@ async fn main() -> std::io::Result<()> {
         .parse()
         .expect("PORT must be a number");
 
-    log::info!("Starting Web Server {}:{}", host, port);
+    // get workers from environment variable and must in integer
+    // and must be greater than 0 and by default is 2
+    let workers: usize = env::var("WORKERS")
+        .unwrap_or("2".to_string())
+        .parse()
+        .expect("WORKERS must be a number and greater than 0");
 
-    HttpServer::new(|| {
+    log::info!(
+        "Starting Web Server http://{}:{} with {} workers",
+        host,
+        port,
+        workers
+    );
+
+    let counter = web::Data::new(api::AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .wrap(Logger::new("excample %a %{User-Agent}i"))
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .configure(init_routes)
+            .app_data(counter.clone())
+            .route("/count", web::get().to(api::index))
     })
+    // actix web server will run with 1 worker
+    .workers(workers)
     .bind((host, port))?
     .run()
     .await
